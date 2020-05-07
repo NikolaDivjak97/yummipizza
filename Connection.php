3fe6da150a38eab708612538adda2d690c8a8698
Nikola <dzona97@gmail.com>
2020-05-07 19:25:26 +0200

    initial commit

--- Connection.php
+++ Connection.php
@@ -1,5 +1,6 @@
 <?php 
-$kon=mysqli_connect("localhost","root","","zadatak") or die("Failed to 
+$kon=mysqli_connect("eu-cdbr-west-03.cleardb.net","bbfa3f2e564a0a","7ebf8349","zadatak") or die("Failed to 
 connect to MySQL: " . mysqli_error());
-			
+
+		
 ?>
