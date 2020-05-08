082ab550bd0faeed8c1d0be9184a3f1a36f37715
Nikola <dzona97@gmail.com>
2020-05-08 08:28:49 +0200

    initial commit

--- Connection.php
+++ Connection.php
@@ -1,5 +1,5 @@
 <?php 
-$kon=mysqli_connect("eu-cdbr-west-03.cleardb.net","bbfa3f2e564a0a","7ebf8349","zadatak") or die("Failed to 
+$kon=mysqli_connect("eu-cdbr-west-03.cleardb.net","bbfa3f2e564a0a","7ebf8349","heroku_b67adef3ebe9aff") or die("Failed to 
 connect to MySQL: " . mysqli_error());
 
 		
